3074e517a89c5c09e2ed3bd5f0d075b618a2b7dd
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-10-14 02:03:51 -0300

    feat: create orphanage-map page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Landing} />
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
     </BrowserRouter>
   );
 }
