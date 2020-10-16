87ae2abcfd0981e4bfb4cb010dd6403c21ca0036
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-10-16 17:25:37 -0300

    feat: orphanage description and creation page

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -52,7 +52,7 @@ function OrphanagesMap() {
         >
           <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
             Lar das meninas
-            <Link to="">
+            <Link to="/orphanages/fa-rotate-180">
               <FiArrowRight size={20} color="#FFF" />
             </Link>
           </Popup>
@@ -60,7 +60,7 @@ function OrphanagesMap() {
         
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
